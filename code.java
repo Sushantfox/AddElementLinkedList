b017c8784ee3b00e7326e44cb833ab7b380ab42d
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-13 16:24:56 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+class Main {
+  public static void main(String[] args) {
+    LinkedList<String> mammals = new LinkedList<>();
+
+    mammals.add("Dog");
+    mammals.add("Cat");
+    mammals.add("Horse");
+    System.out.println("Mammals: " + mammals);
+
+    LinkedList<String> animals = new LinkedList<>();
+    animals.add("Crocodile");
+
+  }
+}
